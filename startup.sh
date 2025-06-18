4643d30c2909b4a3c9d4692bc8493c8a3388ed0e
JorisJBackis <joris.backis@student.nhlstenden.com>
2025-06-18 20:34:14 +0200

    feat: add custom startup script to install ffmpeg

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install custom packages
+echo "--- [startup.sh] Running apt-get update ---"
+apt-get update
+echo "--- [startup.sh] Installing ffmpeg ---"
+apt-get install -y ffmpeg
+echo "--- [startup.sh] ffmpeg installation finished ---"
+
+# Start the Gunicorn server
+echo "--- [startup.sh] Starting Gunicorn server ---"
+gunicorn --bind=0.0.0.0:$PORT --timeout 600 --workers=1 --log-level=debug --error-logfile=- --access-logfile=- run:app
